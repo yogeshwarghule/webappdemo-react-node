244799592ec7df3488eafa458451d1579861609f
Royce Ho <royce963@gmail.com>
2022-06-07 15:52:53 +0800

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,121 @@
+import React, { useState } from 'react'
+import './App.css'
+import Axios from 'axios'
+
+function App() {
+  const [name, setName] = useState("")
+  const [description, setDescription] = useState("")
+  const [age, setAge] = useState(0)
+  const [employeeList, setEmployeeList] = useState([])
+
+  // for /updates
+  const [newDescription, setNewDescription] = useState("")
+  const [newAge, setNewAge] = useState(0)
+
+  const addEmployee = () => {
+    const currEmployeeData = {
+      name: name,
+      description: description,
+      age: age
+    }
+    Axios.post("http://localhost:8081/create", currEmployeeData).then(() => {
+      console.log("success")
+      setEmployeeList([...employeeList, currEmployeeData])
+    }
+    )
+  }
+
+  const getEmployees = () => {
+    Axios.get("http://localhost:8081/read").then((res) => {
+      setEmployeeList(res.data)
+    })
+  }
+
+  const deleteEmployee = (id) => {
+    Axios.delete(`http://localhost:8081/delete/${id}`).then((response) => {
+      setEmployeeList(
+        employeeList.filter((val) => {
+          return val.id !== id;
+        })
+      )
+    })
+  }
+
+  const updateEmployee = (id) => {
+    Axios.put("http://localhost:8081/update", { id: id, age: newAge, description: newDescription }).then(
+      (response) => {
+        setEmployeeList(
+          employeeList.map((val) => {
+            return val.id === id
+              ? {
+                id: val.id,
+                name: val.name,
+                description: newDescription,
+                age: newAge
+              } : val
+          })
+        )
+      }
+    )
+  }
+
+  return (
+    <div className='App'>
+      <div className='info'>
+        <label>Name: </label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+          required
+        />
+        <label>Description: </label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setDescription(event.target.value);
+          }}
+          required
+        />
+        <label>Age: </label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+          required
+        >
+
+        </input>
+        <button onClick={addEmployee}>Add Employee
+        </button>
+        <button onClick={getEmployees}>Show Employees</button>
+        <div>
+
+          {employeeList.map((val, key) => {
+            return (
+              <div className='employeeListing'>
+                <div>
+                  <h3>ID: {val.id}, Name: {val.name},
+                    Description: {val.description},
+                    Age: {val.age}
+                  </h3>
+                  <div>
+                  <input type="text" onChange={(event) => setNewDescription(event.target.value)} placeholder="Enter New Desc..." required/>
+                  <input type="number" onChange={(event) => setNewAge(event.target.value)} placeholder="Enter New Age..." required/>
+
+                  </div>
+                  <button onClick={() => updateEmployee(val.id)}>Update</button>
+                  <button onClick={() => deleteEmployee(val.id)}>Delete</button>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
