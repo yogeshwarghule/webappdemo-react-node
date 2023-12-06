682ed46422b6b05a9b8c3f61af9a7918d0443522
Yogeshwar Ghule <yghule2001@gmail.com>
2023-12-06 19:33:43 +0530

    Delete config directory

--- config/mysql-ec2-user-data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt update
-sudo apt install -y mysql-server
-echo "install done"
-sudo mysql -e "CREATE DATABASE employee_db;"
-sudo mysql -e "USE employee_db; CREATE TABLE employees ( id INT NOT NULL AUTO_INCREMENT, name VARCHAR(150) NOT NULL, description VARCHAR(150) NOT NULL, age INT NOT NULL, PRIMARY KEY (id) );"
-sudo mysql -e "CREATE USER 'myuser'@'localhost' IDENTIFIED BY 'mypass'; CREATE USER 'myuser'@'%' IDENTIFIED BY 'mypass'; GRANT ALL ON *.* TO 'myuser'@'localhost'; GRANT ALL ON *.* TO 'myuser'@'%';
-FLUSH PRIVILEGES;"
-sudo service mysql restart
